6018dd3b1fd1919e48c3b687b5cd0d705187ab36
Bino Maiheu <binomaiheu@gmail.com>
2015-12-22 10:34:36 +0100

    added units to the time interval

--- src/opaq_core/TimeInterval.cpp
+++ src/opaq_core/TimeInterval.cpp
@@ -9,9 +9,33 @@
 
 namespace OPAQ {
 
-TimeInterval::TimeInterval() {}
+TimeInterval::TimeInterval() {
+	_seconds = 0;
+}
 
 TimeInterval::~TimeInterval() {}
+
+void TimeInterval::TimeInterval( long value, TimeInterval::Unit unit ) {
+
+	switch( unit ) {
+	case TimeInterval::Seconds:
+		this->_seconds = value;
+		break;
+    case TimeInterval::Minutes:
+    	this->_seconds = 60*value;
+    	break;
+    case TimeInterval::Hours:
+    	this->_seconds = 3600*value;
+    	break;
+    case TimeInterval::Days:
+    	this->_seconds = 86400*value;
+    	break;
+    default:
+    	throw RunTimeException( "wrong time interval given" );
+    }
+	return;
+}
+
 
 const TimeInterval TimeInterval::operator+(const TimeInterval &other) const {
 	TimeInterval out = *this;
@@ -24,5 +48,8 @@ const TimeInterval TimeInterval::operator-(const TimeInterval &other) const {
 	out._seconds -= other.getSeconds();
 	return out;
 }
+
+
+
 
 } /* namespace OPAQ */
