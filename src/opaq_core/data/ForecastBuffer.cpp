63d5548d3dd1141ca91b3c64d1b0e97dac29df39
Bino Maiheu <binomaiheu@gmail.com>
2015-12-23 12:00:57 +0100

    accomodated new ForecastBuffer and removed ForecastHorizon refs

---
+++ src/opaq_core/data/ForecastBuffer.cpp
@@ -0,0 +1,18 @@
+/*
+ * ForecastBuffer.cpp
+ *
+ *  Created on: 2015
+ *      Author: bino.maiheu@vito.be
+ */
+
+#include "ForecastBuffer.h"
+
+namespace OPAQ {
+
+ForecastBuffer::ForecastBuffer() {
+}
+  
+ForecastBuffer::~ForecastBuffer() {
+}
+
+} /* namespace OPAQ */
