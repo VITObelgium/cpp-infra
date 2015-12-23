b784b887009856d54081c926eef44332dd593918
Bino Maiheu <binomaiheu@gmail.com>
2015-12-23 12:05:36 +0100

    Accomodated new data provider structure, with absolute date/times

--- src/plugins/data/src/RioObsProvider.h
+++ src/plugins/data/src/RioObsProvider.h
@@ -28,100 +28,51 @@ public:
   
   /**
    * RioObsProvider configuration:
-   * <!-- range to buffer, offsets relative to base time
-   *  expressed in hours -->
-   * <range>
-   * 	<begin_offset>-48</begin_offset>
-   * 	<end_offset>0</end_offset>
-   * 	<clear_on_new_basetime>false</clear_on_new_basetime>	<!-- true or false, optional: default = true -->
-   * </range>
+   *
    * <file_pattern>/full/path/to/data/file/name_of_file_for_pollutant_%POL%_.txt</file_pattern>
-	 */
+   */
+
   virtual void configure(TiXmlElement * configuration)
     throw (BadConfigurationException);
   
   // OPAQ::DataProvider methods
-  
-  virtual void setAQNetworkProvider(AQNetworkProvider * aqNetworkProvider);
-  
-  virtual void setBaseTime (const DateTime & baseTime) throw (BadConfigurationException);
-  
   virtual TimeInterval getTimeResolution();
-  
-  virtual std::pair<const TimeInterval, const TimeInterval> getRange();
-  
-  virtual std::pair<const TimeInterval, const TimeInterval> getRange(const ForecastHorizon & forecastHorizon);
-  
-  virtual unsigned int size();
-  
+
   virtual double getNoData();
-  
-  /**
-   * The id parameter is ignored
-   */
-  virtual double getNoData(const std::string & id);
-  
-  virtual std::vector<double> getValues(const TimeInterval & beginOffset,
-					const TimeInterval & endOffset, 
-					const std::string & pollutant,
-					const std::string & station);
-  
-  virtual std::vector<double> getValues(const std::string & modelName, 
-					const TimeInterval & beginOffset,
-					const TimeInterval & endOffset, 
-					const std::string & pollutant,
-					const std::string & station) {
-    throw RunTimeException( "RioObsProvider only for data, no valid model name" );
-  }
 
   /**
-   * The forecastHorizon parameter will be ignored.
+   * Simply return the observations for the station & pollutant Id between the given dates
+   * the aggregation type returns what aggregation to give, default (when the 4th argument is
+   * omitted, the base resolution is returned)
    */
-  virtual std::vector<double> getValues(const TimeInterval & beginOffset,
-					const TimeInterval & endOffset, 
-					const std::string & pollutant,
-					const std::string & station,
-					const ForecastHorizon & forecastHorizon);
+  virtual OPAQ::TimeSeries<double> getValues( const DateTime& t1, const DateTime& t2,
+		  const std::string& stationId, const std::string& pollutantId,
+		  OPAQ::Aggregation::Type aggr = OPAQ::Aggregation::None );
   
-  virtual std::vector<double> getValues(const std::string & modelName, 
-					const TimeInterval & beginOffset,
-					const TimeInterval & endOffset, 
-					const std::string & pollutant,
-					const std::string & station,
-					const ForecastHorizon & forecastHorizon) {
-    throw RunTimeException( "RioObsProvider only for data, no valid model name" );
-  }
-  
-  /**
-   * The forecastHorizon parameter will be ignored
-   */
-  virtual std::vector<double> getValues(const std::string & pollutant,
-					const TimeInterval & offset = TimeInterval(0),
-					const ForecastHorizon & forecastHorizon = ForecastHorizon(0));
-
 private:
-  double _noData;
-  TimeInterval _timeResolution;
-  DateTime _begin, _end;
-  // pollutant -> (station -> data)
-  std::map<std::string, std::map<std::string, std::vector<double> > > _buffer;
-  std::vector<double> _empty;
-  std::string _pattern;
-  AQNetworkProvider * _aqNetworkProvider;
-  DateTime _baseTime;
-  long _beginOffset, _endOffset;
-  bool _configured, _baseTimeSet, _clearBufferOnBaseTimeReset;
+  double              _noData;
+  TimeInterval        _timeResolution;
+  std::string         _pattern;
+  bool                _configured;
+
+  int                 _nvalues; //< number of values on a line
+
+  // order of the map :
+  // pollutant -> ( aggregation -> ( station -> data ) )
+
+  // buffer for the aggregations in the RIO files, includes the raw as well
+  // note that in the RIO files, these aggregations are pre-calculated, but this
+  // does not necessarily have to be the case...
+  // station --> pollutant --> aggregation --> data
+  std::map< std::string, std::map<OPAQ::Aggregation::Type, std::map<std::string, OPAQ::TimeSeries<double> > > >_buffer; //< the data buffer for the aggregations
+
+  OPAQ::TimeSeries<double> *_getTimeSeries( const std::string& pollutant,
+		  const std::string& station, OPAQ::Aggregation::Type aggr );
   
-  void _checkFullyConfigured() throw (NotConfiguredException);
+  void _readFile ( const std::string & pollutant );
   
-  void _calculateBeginEnd ();
-  
-  std::vector<double> & _getValues(const std::string & pollutant,
-			const std::string & station);
-  
-  void _readFile (const std::string & pollutant);
-  
-  std::vector<double> * _getOrInitValues(const std::string & pollutant, const std::string & station);
+  OPAQ::TimeSeries<double> *_getOrInitValues( const std::string & pollutant,
+		  OPAQ::Aggregation::Type aggr, const std::string & station );
 };
   
 } /* namespace OPAQ */
