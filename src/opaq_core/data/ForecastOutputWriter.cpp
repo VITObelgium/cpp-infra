ba0e57f938cbbcee608aa082bc03933bbaef1949
Bino Maiheu <binomaiheu@gmail.com>
2015-12-23 12:03:44 +0100

    accomodated new ForecastBuffer and removed ForecastHorizon refs

--- src/opaq_core/data/ForecastOutputWriter.cpp
+++ src/opaq_core/data/ForecastOutputWriter.cpp
@@ -3,6 +3,10 @@
 namespace OPAQ {
 
 ForecastOutputWriter::ForecastOutputWriter(){
+	_net        = NULL;
+	_fcHor      = NULL;
+	_modelNames = NULL;
+	_buf        = NULL;
 }
 
 ForecastOutputWriter::~ForecastOutputWriter(){
