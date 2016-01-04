afe2f58d42d678b51ecfcf289eb9a59730ba7d8f
Bino Maiheu <binomaiheu@gmail.com>
2016-01-04 09:30:19 +0100

    added functionality to parse the tunes

--- src/plugins/forecast/src/OVL.cpp
+++ src/plugins/forecast/src/OVL.cpp
@@ -18,7 +18,63 @@ void OVL::configure (TiXmlElement * cnf )
 
 	// here the actual station configuation & RTC configuration should be read, the individual models
 	// are already defined... in there respective plugins...
+	_conf.clear();
 
+	try {
+		// read the mode for which the corresponding tune should be taken
+		this->tune_mode = XmlTools::getText(cnf, "tune_mode" );
+
+		// read missing value for this model
+		this->missing_value = atoi(XmlTools::getText( cnf, "missing_value" ).c_str() );
+
+		// parse the tunes database & run configuration for OVL
+		TiXmlElement *tuneEl = XmlTools::getElementByAttribute( cnf->FirstChildElement( "tunes" ), "tune", "mode", this->tune_mode );
+		_parseTunes( tuneEl );
+
+	} catch ( BadConfigurationException & err ) {
+		throw BadConfigurationException(err.what());
+	}
+
+}
+
+
+void OVL::_parseTunes( TiXmlElement *lst ) {
+
+	TiXmlElement *polEl = lst->FirstChildElement( "pollutant" );
+	while( polEl ) {
+
+		std::string polName = polEl->Attribute( "name" );
+		std::string polAggr = polEl->Attribute( "aggr" );
+
+		TiXmlElement *stEl = polEl->FirstChildElement( "station" );
+		while ( stEl ) {
+
+			std::string stName = stEl->Attribute( "name" );
+
+			TiXmlElement *modelEl = stEl->FirstChildElement( "model" );
+			while( modelEl ) {
+				OVL::StationConfig c;
+				int fc_hor;
+
+				modelEl->QueryIntAttribute( "fc_hor", &fc_hor );
+				modelEl->QueryIntAttribute( "rtc_mode", &(c.rtc_mode) );
+				modelEl->QueryIntAttribute( "rtc_param", &(c.rtc_param) );
+				c.model_name = modelEl->GetText();
+
+				// std::make_tuple( "pm10", "dayavg", "40ML01", TimeInterval( 1, day ) )
+				// inserting using list initializer instead of std::pair()... doesnt work for replacing the std::make_tuple command though
+				_conf.insert( { std::make_tuple( polName, polAggr, stName, fc_hor ), c } );
+
+				modelEl = modelEl->NextSiblingElement( "model" );
+			}
+
+			stEl = stEl->NextSiblingElement( "station" );
+		} // loop over the stations
+
+		polEl = polEl->NextSiblingElement( "pollutant" );
+	} // loop over the pollutants
+
+	return;
 }
 
 
@@ -39,6 +95,8 @@ void OVL::run() {
 
 	std::vector<Station *> stations = net->getStations();
 
+	OPAQ::Aggregation::Type agg = OPAQ::Aggregation::DayAvg;
+
 	// -- Forecast horizon
 	// forecast horizon requested by user is available in abstract model and
 	// defined in the configuration file under <forecast><horizon></horizon></forecast>
@@ -58,7 +116,6 @@ void OVL::run() {
 		} else
 			logger->trace("Forecasting station " + station->getName() );
 
-
 		// store the output in a timeseries object
 		OPAQ::TimeSeries<double> fc;
 		fc.clear();
@@ -68,11 +125,43 @@ void OVL::run() {
 			OPAQ::TimeInterval fcHor( fc_hor, TimeInterval::Days );
 			DateTime fcTime = baseTime + fcHor;
 
-			// import the model plugin, but cast it as a MLP_FeedForwardModel... instead of a Model is that possible ??? YES !!!! moehaha !!
-			std::string modelName = "OVL_model2_9UT"; // has to be the component name in the xml !!!
-			MLP_FeedForwardModel *model = ComponentManager::getInstance()->getComponent<MLP_FeedForwardModel>( modelName );
+			// lookup the model
+			// TODO : put in the correct aggregation, where to get it from ??
+			auto modelIt = _conf.find( std::make_tuple( pol.getName(), OPAQ::Aggregation::getName(agg), station->getName(), fc_hor ) );
+			if (modelIt == _conf.end() ) {
+				// no configuration for this station, returning -9999 or something
+				logger->warn(  "Model configuration not found for " + pol.getName() + ", st = " + station->getName() + ", skipping..."   );
+				fc.insert( fcTime, this->missing_value );
+				continue;
+			}
 
+
+#ifdef DEBUG
+			std::cout << "Constructing model " << modelIt->second.model_name << " for " << pol.getName() << ", st = " << station->getName() << ", fc_hor = " << fcHor.getDays()
+					  << "rtc_mode = " << modelIt->second.rtc_mode << ", param = " << modelIt->second.rtc_param << std::endl;
+#endif
+
+			// get the correct model plugin, we don't have to destroy it as there is only one instance of each component,
+			// configuration via the setters...
+			MLP_FeedForwardModel *model = ComponentManager::getInstance()->getComponent<MLP_FeedForwardModel>( modelIt->second.model_name );
+
+			// set ins and outs for the model here...
+			// this is in fact a small engine...
+			model->setBaseTime(baseTime);
+			model->setPollutant( pol );
+			model->setAQNetworkProvider( getAQNetworkProvider() );
+			model->setForecastHorizon( getForecastHorizon() );
+			model->setInputProvider( getInputProvider() );
+			model->setMeteoProvider( getMeteoProvider() );
+			model->setBuffer( getBuffer() );
+
+			// run the model
 			double out = model->fcValue( pol, *station, OPAQ::Aggregation::DayAvg, baseTime, fcHor );
+
+			// TODO now handle the RTC !!!
+
+
+
 
 			fc.insert( fcTime, out );
 		}
