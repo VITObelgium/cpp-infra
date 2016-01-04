43168a722e847a76ab8c432b76c5224d1f4abea6
Bino Maiheu <binomaiheu@gmail.com>
2016-01-04 09:30:18 +0100

    added functionality to parse the tunes

--- src/plugins/forecast/src/OVL.h
+++ src/plugins/forecast/src/OVL.h
@@ -13,6 +13,8 @@
 #define OVL_H_
 
 #include <string>
+#include <map>
+#include <tuple>
 #include <opaq.h>
 
 namespace OPAQ {
@@ -31,8 +33,33 @@ public:
     virtual void run();
     
 
+    // define a nested struct to hold the configuration of
+    // a station
+    struct StationConfig {
+    	int         rtc_mode;
+    	int         rtc_param;
+    	std::string model_name;
+    };
+
+
 private:
     LOGGER_DEC();
+
+    std::string tune_mode;       //! the selected tune mode for how OVL was optimized.
+    int         missing_value;   //! a missing value
+
+    void _parseTunes( TiXmlElement *lst );
+
+    /**
+     * this one is a map for
+     * pollutant -> aggregation, followed by a list of OVL::StationConfig structures
+     * using std::tuples for this, a key pair can be made via
+     * std::make_tuple( "pm10", "dayavg", "40ML01", fc_hor )
+     * where fc_hor is the forecast horizon in days...
+     * we are not using TimeInterval in the tuple (could do) because we would need to
+     * add some stuff in the implementation of the TimeInterval to be able to use it in a tuple
+     */
+    std::map<std::tuple<std::string, std::string, std::string, int>, OVL::StationConfig> _conf;
   };
   
   
