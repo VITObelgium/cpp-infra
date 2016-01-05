770d6b3253c4fabce390843054187b7b0935fa42
Bino Maiheu <binomaiheu@gmail.com>
2016-01-05 16:21:34 +0100

    added aggregation to sample creating routine and added more error checking on the constructed input sample

--- src/plugins/forecast/src/OVL_IRCEL_model1.cpp
+++ src/plugins/forecast/src/OVL_IRCEL_model1.cpp
@@ -3,6 +3,8 @@
 #include <opaq.h>
 #include "OVL_IRCEL_model1.h"
 
+#define epsilon 1e-6
+
 // a list of meteo variable names relevant for this model
 // we should acutally better put this in the config file and allow the user
 // to configure the ff model feature vector from the configuration
@@ -17,10 +19,10 @@ namespace OPAQ {
   OVL_IRCEL_model1::OVL_IRCEL_model1() {
     missing_value = -9999; // set default missing value, overwritting in xml config
     mor_agg       = -1;
+    sample_size   =  2;
   }
 
   OVL_IRCEL_model1::~OVL_IRCEL_model1() {};
-
 
   /* ============================================================================
      Implementation of the configure method
@@ -51,58 +53,63 @@ namespace OPAQ {
   /* ============================================================================
      Construct sample for the OVL_IRCEL_model1 configuration
      ========================================================================== */
-  int OVL_IRCEL_model1::makeSample( double *sample, const OPAQ::Station& st, const OPAQ::Pollutant& pol,
-				    const OPAQ::DateTime &baseTime, const OPAQ::DateTime &fcTime,
-				    const OPAQ::TimeInterval &fc_hor ) {
+  int OVL_IRCEL_model1::makeSample( double *sample, const OPAQ::Station& st,
+		  const OPAQ::Pollutant& pol, Aggregation::Type aggr,
+		  const OPAQ::DateTime &baseTime, const OPAQ::DateTime &fcTime,
+		  const OPAQ::TimeInterval &fc_hor ) {
 
-	OPAQ::DateTime t1, t2;
 	int have_sample = 0; // return code, 0 for success
+	OPAQ::DateTime t1, t2;
 
     // -----------------------
     // Getting data providers --> stored in main model...
     // -----------------------
-    DataProvider *obs   = getInputProvider();
+    DataProvider *obs    = getInputProvider();
     MeteoProvider *meteo = getMeteoProvider();
     
     // -----------------------
     // Get the meteo input
     // -----------------------
-
     // BLH for dayN, offsets relative from fcTime (set by setBaseTime in the meteo provider)
     t1 = fcTime + OPAQ::TimeInterval(0);
     t2 = fcTime + OPAQ::TimeInterval(24, TimeInterval::Hours ) - meteo->getTimeResolution();
     TimeSeries<double> blh  = meteo->getValues( t1, t2, st.getMeteoId(), p_blh );
 
-    // write some debugging output to a file ...
-    /*
-    std::ofstream fs;
-    fs.open( std::string( "debug_" ) + st->getName() + "_" + pol->getName() + "_"
-    		+ std::to_string( baseTime.getYear() ) + "-"
-    		+ std::to_string( baseTime.getMonth() ) + "-"
-			+ std::to_string( baseTime.getDay() )
-    		+ ".txt" );
-    fs << st->getMeteoId() << std::endl;
-    fs << blh;
-    fs.close();
-     */
-    
-    // -----------------------------
-    // get the morning concentration
-    // -----------------------------
-    t1 = DateTimeTools::floor( baseTime, DateTimeTools::FIELD_DAY );
-    t2 = t1 + OPAQ::TimeInterval( this->mor_agg-1, TimeInterval::Hours ); // mor_agg uur of eentje aftrekken ?
-
-    OPAQ::TimeSeries<double> xx_morn = obs->getValues(t1, t2, st.getName(), pol.getName() ); // no aggregation
-
     // -----------------------
     // build sample
     // -----------------------
     
-    // add some more checks for missing values,  if sample not complete... either estimate value
-    // from climatology ?? or don't run forecast...
-    
-    sample[0] = log(1 + mean_missing( xx_morn.values(), obs->getNoData() ) );      // PMMOR
-    sample[1] = log(1 + mean_missing( blh.values(), meteo->getNoData( p_blh ) ) );  // BLH
+    // 0. -------------------------------------------------------------------------------
+    // sample[0] is the mean morning concentration of the measured pollutant we're trying to forecast
+    t1 = DateTimeTools::floor( baseTime, DateTimeTools::FIELD_DAY );
+    t2 = t1 + OPAQ::TimeInterval( this->mor_agg-1, TimeInterval::Hours ); // mor_agg uur of eentje aftrekken ?
+
+    OPAQ::TimeSeries<double> xx_morn = obs->getValues(t1, t2, st.getName(), pol.getName() ); // no aggregation
+    double xx = mean_missing( xx_morn.values(), obs->getNoData() );
+    if ( fabs( xx - obs->getNoData() ) > epsilon ) sample[0] = log(1 + xx );
+    else have_sample++;
+
+
+    // 1. -------------------------------------------------------------------------------
+    // sample[1] is the mean/min/max boundary layer height of the forecast day, depending on
+    // the aggregation time & polluant.
+    if ( aggr == OPAQ::Aggregation::Max1h ) {
+    	if ( ! pol.getName().compare("o3") ) {
+    		// take the maximum BLH for O3 forecasts
+    		xx = max_missing( blh.values(), meteo->getNoData( p_blh ) );
+    	} else {
+    		// take the minimum BLH
+    		xx = min_missing( blh.values(), meteo->getNoData( p_blh ) );
+    	}
+    } else {
+    	// also for max8h we use the daily averages
+    	xx = mean_missing( blh.values(), meteo->getNoData( p_blh ) );
+    }
+    if ( fabs( xx - meteo->getNoData(p_blh) ) > epsilon ) sample[1] = log(1 + xx );
+    else {
+    	have_sample++;
+    	// TODO perhaps provide some kind of climatology to fill the missing sample...
+    }
     
     return have_sample;
   }
