e6833c13f2a29eb43487012c398a3836f63ee95c
Bino Maiheu <binomaiheu@gmail.com>
2016-01-05 13:45:30 +0100

    moved the ffnet name to the MLP_FeedForwardModel base class to avoid duplication, pattern still needs to be set in the daughter model classed by the configure method

--- src/plugins/forecast/src/OVL_IRCEL_model1.cpp
+++ src/plugins/forecast/src/OVL_IRCEL_model1.cpp
@@ -11,20 +11,12 @@ const std::string p_wsp10m  = "P03"; // wind speed 10 m
 const std::string p_wdir10m = "P04"; // wind direction 10 m 
 const std::string p_blh     = "P07"; // boundary layer height
 const std::string p_cc      = "P12"; // total cloud cover
-
   
-// define placeholders for the neural network input files
-const std::string POLLUTANT_PLACEHOLDER = "%pol%";     // placeholder for the pollutant in config file
-const std::string STATION_PLACEHOLDER   = "%station%"; // placeholder for the station in config file
-const std::string FCHOR_PLACEHOLDER     = "%fc_hor%";   // idem for forecast horizon
-const std::string MODEL_PLACEHOLDER     = "%model%";   // idem for feature vector model
-const std::string MCID_PLACEHOLDER      = "%mcid%";   //  idem for the morning Id : 9UT, 7CST etc...
-
 namespace OPAQ {
 
   OVL_IRCEL_model1::OVL_IRCEL_model1() {
     missing_value = -9999; // set default missing value, overwritting in xml config
-    mor_agg       = 9;
+    mor_agg       = -1;
   }
 
   OVL_IRCEL_model1::~OVL_IRCEL_model1() {};
@@ -37,15 +29,8 @@ namespace OPAQ {
     throw (BadConfigurationException) {
     
     try {
-      
       // read the path to the architecture files
       this->pattern = XmlTools::getText(cnf, "ffnetfile_pattern" );
-      
-      // read morning aggregation 
-      this->mor_agg = atoi(XmlTools::getText( cnf, "mor_agg" ).c_str() );
-      
-      // read matching id
-      this->mcid = XmlTools::getText( cnf, "mcid");
 
       // read missing value for this model
       this->missing_value = atoi(XmlTools::getText( cnf, "missing_value" ).c_str() );
@@ -54,27 +39,14 @@ namespace OPAQ {
       throw BadConfigurationException(e.what());
     }
     
+    // read morning aggregation (optional)
+    try {
+    	this->mor_agg = atoi(XmlTools::getText( cnf, "mor_agg" ).c_str() );
+    } catch (ElementNotFoundException & e) {
+    	this->mor_agg = 24; // take all available observations for the day
+    }
+
   }
-
-  /* ============================================================================
-     Constructs the filename for the neural network parameters
-     ========================================================================== */
-  std::string OVL_IRCEL_model1::getFFNetFile( const std::string &pol_name, 
-					      const std::string &st_name, 
-					      int fc_hor ) {
-
-    // Building filename... 
-    std::string fname = this->pattern;
-    OPAQ::StringTools::replaceAll( fname, POLLUTANT_PLACEHOLDER, pol_name );
-    OPAQ::StringTools::replaceAll( fname, STATION_PLACEHOLDER, st_name );
-    OPAQ::StringTools::replaceAll( fname, FCHOR_PLACEHOLDER, std::to_string(fc_hor) );
-    OPAQ::StringTools::replaceAll( fname, MODEL_PLACEHOLDER, this->getName() );
-    OPAQ::StringTools::replaceAll( fname, MCID_PLACEHOLDER,  this->mcid );
-    
-    return fname;
-  }
-
-
   
   /* ============================================================================
      Construct sample for the OVL_IRCEL_model1 configuration
@@ -113,18 +85,14 @@ namespace OPAQ {
     fs << blh;
     fs.close();
      */
-
-    // TODO check the number of
-    std::cout << "TODO : check what is returned by the meteo getter" << std::endl;
     
     // -----------------------------
     // get the morning concentration
     // -----------------------------
     t1 = DateTimeTools::floor( baseTime, DateTimeTools::FIELD_DAY );
-    t2 = t1 + OPAQ::TimeInterval( this->mor_agg-1, TimeInterval::Hours );// met mor_agg uur of eentje aftrekken ????
+    t2 = t1 + OPAQ::TimeInterval( this->mor_agg-1, TimeInterval::Hours ); // mor_agg uur of eentje aftrekken ?
 
     OPAQ::TimeSeries<double> xx_morn = obs->getValues(t1, t2, st.getName(), pol.getName() ); // no aggregation
-    
 
     // -----------------------
     // build sample
@@ -133,8 +101,8 @@ namespace OPAQ {
     // add some more checks for missing values,  if sample not complete... either estimate value
     // from climatology ?? or don't run forecast...
     
-    sample[0] = log(1 + mean_missing( xx_morn.values(), obs->getNoData() ) );     // PMMOR
-    sample[1] = log(1 + mean_missing(blh.values(),meteo->getNoData( p_blh ) ) );  // BLH
+    sample[0] = log(1 + mean_missing( xx_morn.values(), obs->getNoData() ) );      // PMMOR
+    sample[1] = log(1 + mean_missing( blh.values(), meteo->getNoData( p_blh ) ) );  // BLH
     
     return have_sample;
   }
