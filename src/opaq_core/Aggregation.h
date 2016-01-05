00930db354dd93e7224c15606e15c6d28a1ad706
Bino Maiheu <binomaiheu@gmail.com>
2016-01-05 11:56:04 +0100

    added fromString function

--- src/opaq_core/Aggregation.h
+++ src/opaq_core/Aggregation.h
@@ -9,6 +9,7 @@
 #define SRC_OPAQ_CORE_AGGREGATION_H_
 
 #include <string>
+#include "Exceptions.h"
 
 namespace OPAQ {
 
@@ -19,7 +20,21 @@ public:
 
 	enum Type { None, DayAvg, Max1h, Max8h };
 
+	/**
+	 * Returns the preferred name of the aggregation type
+	 */
 	static std::string getName( Aggregation::Type agg );
+
+	/**
+	 * Converts the string to the aggregation type, supports the old-school
+	 * ones as well and is case in-sensitive. Supported are :
+	 * - da, dayavg, dailyavg        -> OPAQ::AggregationType::DayAvg
+	 * - m1, max1h, daymax, dailymax -> OPAQ::AggregationType::Max1h
+	 * - m8, max8h                   -> OPAQ::AggregationType::Max8h
+	 *
+	 * \param s input string
+	 */
+	static Aggregation::Type fromString( std::string s ) throw ( NotAvailableException ) ;
 };
 
 } /* namespace OPAQ */
