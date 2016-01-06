8a7fe8c7360f40df0ff3dd0574a91822dcecd1aa
Bino Maiheu <binomaiheu@gmail.com>
2016-01-06 14:20:21 +0100

    added winddir average calculation

--- src/opaq_core/Math.h
+++ src/opaq_core/Math.h
@@ -8,6 +8,8 @@
 #ifndef SRC_OPAQ_CORE_MATH_H_
 #define SRC_OPAQ_CORE_MATH_H_
 
+#include <vector>
+
 namespace OPAQ {
 
 class Math {
@@ -15,7 +17,20 @@ public:
 	Math();
 	virtual ~Math();
 
+	/**
+	 * Guess...
+	 */
 	constexpr static double Pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286209;
+
+	/**
+	 * Computes the average wind direction vector (normalised) for the given wdir's (in degrees). The routine
+	 * \param *x_vec  points to the x component to be returned
+	 * \param *x_vec  points to the y component to be returned
+	 * \param wdir    is the vector of wind directions
+	 * \param missing is a missing value which can occurr in the vector of wind directions
+	 * \param ok      returns true if succesfull, false if e.g. nothing was found
+	 */
+	static void winddir( double *x_vec, double *y_vec, const std::vector<double>& wdir, double missing, bool *ok );
 };
 
 } /* namespace OPAQ */
