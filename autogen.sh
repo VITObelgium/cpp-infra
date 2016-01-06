0e2f072b3941ac514ee0d324fd88e4b5493805fb
Bino Maiheu <binomaiheu@gmail.com>
2016-01-06 14:57:29 +0100

    leave it up to the GNU autoreconf to do the configuration, works better for the m4 macros

--- autogen.sh
+++ autogen.sh
@@ -3,26 +3,7 @@
 echo "Bootstrapping OPAQ"
 rm -rf autom4te.cache
 
-# libtoolize is called glibtoolize on osx.. check this !
-echo " -- library has libtool support, libtoolizing..."
-syst=$(uname)
-if [ ${syst} = "Darwin" ]; then
-    glibtoolize --copy --force > /dev/null
-else
-    libtoolize --copy --force > /dev/null
-fi
-
-echo " -- running aclocal..."
-aclocal  
-
-echo " -- running autoheader..."
-autoheader
-
-echo " -- running automake, creating Makefile template..."
-automake --copy --add-missing --force-missing --foreign
-
-echo " -- running autoconf, creating configure scripts..."
-autoconf --force
+autoreconf -fvi
 
 echo "All done :)"
 
