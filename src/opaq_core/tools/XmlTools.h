a6eda7cb4ded38659bdabbcd1c80785d50a6a9f6
Bino Maiheu <binomaiheu@gmail.com>
2016-01-07 13:09:33 +0100

    added find element by multiple attributes mehod as well as support for using ref= files

--- src/opaq_core/tools/XmlTools.h
+++ src/opaq_core/tools/XmlTools.h
@@ -19,10 +19,9 @@ public:
 	XmlTools();
 	virtual ~XmlTools();
 
-	static std::string getText(TiXmlElement * parent,
-			const std::string & childName)
-					throw (ElementNotFoundException) {
-		TiXmlElement * child = getElement(parent, childName);
+	static std::string getText(TiXmlElement* parent, const std::string & childName) {
+
+		TiXmlElement* child = getElement(parent, childName);
 		return std::string(child->GetText());
 	}
 
@@ -32,9 +31,8 @@ public:
 	 * @param childName name of the child element
 	 * @param refDoc pointer to TiXmlDocument to use for the file ref. If this is set, the method will search for the 'ref' attribute in the child and if it is present, it will read the child element from the file given by the value of the ref attribute
 	 */
-	static TiXmlElement * getElement(TiXmlElement * parent,
-			const std::string & childName,
-			TiXmlDocument * refDoc = NULL) throw (ElementNotFoundException);
+	static TiXmlElement* getElement(TiXmlElement* parent, const std::string& childName,
+			TiXmlDocument* refDoc = NULL );
 
 	/**
 	 * Fetch a child element from a given parent element
@@ -43,11 +41,24 @@ public:
 	 * @param childName  name of child element
 	 * @param attrName   name of the attribute
 	 * @param attrValue  value of the attribute
+	 * @param refDoc pointer to TiXmlDocument to use for the file ref. If this is set, the method will search for the 'ref' attribute in the child and if it is present, it will read the child element from the file given by the value of the ref attribute
 	 */
-	static TiXmlElement * getElementByAttribute( TiXmlElement *parent,
-			const std::string &childName, const std::string & attrName,
-			const std::string & attrValue )
-	throw (ElementNotFoundException);
+	static TiXmlElement* getElementByAttribute( TiXmlElement* parent, const std::string& childName,
+			const std::string& attrName, const std::string& attrValue,
+			TiXmlDocument* refDoc = NULL );
+
+	/**
+	 * Fetch a child element from a given parent element
+	 * where an list of attributes have a certain value...
+	 * @param parent     parent element
+	 * @param childName  name of child element
+	 * @param attrName   vector of names for the attributes
+	 * @param attrValue  vector of values for the attributes
+	 * @param refDoc pointer to TiXmlDocument to use for the file ref. If this is set, the method will search for the 'ref' attribute in the child and if it is present, it will read the child element from the file given by the value of the ref attribute
+	 */
+	static TiXmlElement* getElementByAttributes( TiXmlElement *parent, const std::string &childName,
+			const std::vector<std::string>& attrNames, const std::vector<std::string>& attrValues,
+			TiXmlDocument* refDoc = NULL );
 
 };
 
