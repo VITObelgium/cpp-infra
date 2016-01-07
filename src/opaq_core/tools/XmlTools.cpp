d46fcec363d7bceabeea06f88952bff6a32f34c2
Bino Maiheu <binomaiheu@gmail.com>
2016-01-07 13:09:31 +0100

    added find element by multiple attributes mehod as well as support for using ref= files

--- src/opaq_core/tools/XmlTools.cpp
+++ src/opaq_core/tools/XmlTools.cpp
@@ -7,6 +7,19 @@
 
 #include "XmlTools.h"
 
+// small helper function to check for the mattching of attributes
+bool _checkMatch( TiXmlElement *el, const std::vector<std::string>& attrNames, const std::vector<std::string>& attrValues ) {
+
+	unsigned int matches = 0;
+	for ( unsigned int i = 0; i < attrNames.size(); i++ ) {
+		const char *val = el->Attribute( attrNames[i].c_str() );
+		if ( val )
+			if ( ! attrValues[i].compare( attrValues[i] ) ) matches++;
+	}
+	return ( matches == attrNames.size() );
+
+}
+
 namespace OPAQ {
 
 XmlTools::XmlTools() {
@@ -15,34 +28,127 @@ XmlTools::XmlTools() {
 XmlTools::~XmlTools() {
 }
 
-TiXmlElement* XmlTools::getElementByAttribute( TiXmlElement *parent,
-		const std::string &childName, const std::string & attrName, const std::string & attrValue )
-	throw ( ElementNotFoundException ) {
+TiXmlElement* XmlTools::getElementByAttribute( TiXmlElement* parent, const std::string& childName,
+		const std::string& attrName, const std::string& attrValue,
+		TiXmlDocument* refDoc ) {
 
 	if ( ! parent ) throw ElementNotFoundException("parent element does not exist");
 
 	TiXmlElement *el = parent->FirstChildElement( childName );
+	if (! el ) throw ElementNotFoundException("Child with name " + childName + " not found");
+
 	while( el ) {
 
-		const char *val = el->Attribute( attrName.c_str() );
-		if ( val ) {
-			// we have the attribute
-			if ( ! attrValue.compare( val ) ) return el;
+		if (refDoc != NULL) {
+
+			std::string ref;
+			if ( el->QueryStringAttribute("ref", &ref) == TIXML_SUCCESS ) {
+				// ref attribute found
+				if (FileTools::exists(ref)) {
+					// file found
+					if (!refDoc->LoadFile(ref)) {
+						std::stringstream ss;
+						ss << "Failed to load file in ref attribute: " << ref;
+						throw ElementNotFoundException(ss.str());
+					}
+					TiXmlElement* fileElement = refDoc->FirstChildElement(childName);
+					if (!fileElement) {
+						std::stringstream ss;
+						ss << "File in ref attribute (" << ref
+								<< ") does not have '" << childName
+								<< "' as root element";
+						throw ElementNotFoundException(ss.str());
+					}
+
+					const char *val = fileElement->Attribute( attrName.c_str() );
+					if ( val )
+						if ( ! attrValue.compare( val ) ) return fileElement;
+				} else {
+					std::stringstream ss;
+					ss << "File in ref attribute '" << ref << "' not found.";
+					throw ElementNotFoundException(ss.str());
+				}
+			} else {
+				// ref attribute not found
+				const char *val = el->Attribute( attrName.c_str() );
+				if ( val )
+					if ( ! attrValue.compare( val ) ) return el;
+			}
+
+		} else {
+			// -- no ref doc, just check the attribute
+			const char *val = el->Attribute( attrName.c_str() );
+			if ( val )
+				if ( ! attrValue.compare( val ) ) return el;
 		}
 
 		el = el->NextSiblingElement( childName );
 	}
 
+	throw ElementNotFoundException("Did not find child with requested attribute/value pair");
+}
+
+TiXmlElement* XmlTools::getElementByAttributes( TiXmlElement *parent, const std::string &childName,
+		const std::vector<std::string>& attrNames, const std::vector<std::string>& attrValues,
+		TiXmlDocument* refDoc ) {
+
+	if ( ! parent ) throw ElementNotFoundException("parent element does not exist");
+	if ( attrValues.size() != attrNames.size() ) throw InvalidArgumentsException( "attrValues doesnt match attrNames" );
+	if ( attrValues.size() == 0 ) throw InvalidArgumentsException("no attributes given...");
+	unsigned int n = attrValues.size();
+
+	TiXmlElement *el = parent->FirstChildElement( childName );
+	if (! el ) throw ElementNotFoundException("Child with name " + childName + " not found");
+
+	while( el ) {
+
+		if (refDoc != NULL) {
+
+			std::string ref;
+			if ( el->QueryStringAttribute("ref", &ref) == TIXML_SUCCESS ) {
+				// ref attribute found
+				if (FileTools::exists(ref)) {
+					// file found
+					if (!refDoc->LoadFile(ref)) {
+						std::stringstream ss;
+						ss << "Failed to load file in ref attribute: " << ref;
+						throw ElementNotFoundException(ss.str());
+					}
+					TiXmlElement* fileElement = refDoc->FirstChildElement(childName);
+					if (!fileElement) {
+						std::stringstream ss;
+						ss << "File in ref attribute (" << ref
+								<< ") does not have '" << childName
+								<< "' as root element";
+						throw ElementNotFoundException(ss.str());
+					}
+
+					if ( _checkMatch( fileElement, attrNames, attrValues ) ) return fileElement;
+
+
+				} else {
+					std::stringstream ss;
+					ss << "File in ref attribute '" << ref << "' not found.";
+					throw ElementNotFoundException(ss.str());
+				}
+			} else {
+				// ref attribute not found
+				if ( _checkMatch( el, attrNames, attrValues ) ) return el;
+			}
+
+		} else {
+			// -- no ref doc, just check the attribute
+			if ( _checkMatch( el, attrNames, attrValues ) ) return el;
+		}
+
+		el = el->NextSiblingElement( childName );
+	}
 
 	throw ElementNotFoundException("Did not find child with requested attribute/value pair");
 }
 
 
-
-
-TiXmlElement * XmlTools::getElement(TiXmlElement * parent,
-		const std::string & childName, TiXmlDocument * refDoc)
-				throw (ElementNotFoundException) {
+TiXmlElement * XmlTools::getElement(TiXmlElement* parent, const std::string& childName, TiXmlDocument* refDoc) {
 	TiXmlElement * element = parent->FirstChildElement(childName);
 	if (!element) {
 		throw ElementNotFoundException(
