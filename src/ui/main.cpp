0e1bd7df29137816cec5720f5feb2230fa4b646a
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2016-09-30 17:07:26 +0200

    Dynamic qt linking on windows

--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -4,8 +4,10 @@
 #include "config.h"
 #include "mainwindow.h"
 
+#ifdef STATIC_QT
 #if defined WIN32
     Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+#endif
 #endif
 
 int main(int argc, char* argv[])
