334fe2d34efddc12ef6dfb58fe1bc8e86faec752
Dirk Vanden Boer <dirk.vdb@gmail.com>
2016-10-11 20:36:25 +0200

    Use boost for dll function load

--- src/opaq_core/ComponentManager.h
+++ src/opaq_core/ComponentManager.h
@@ -13,12 +13,13 @@
 #include <map>
 #include <memory>
 #include <tinyxml.h>
+#include <boost/shared_ptr.hpp>
 
 /**
  * \brief Macro to register a class as being an OPAQ component.
- * When writing new components, a user should add this macro statement to 
+ * When writing new components, a user should add this macro statement to
  * the implementation source file. Dont forget to include a correct namespace
- * if you want to have the plugin to have it's own namespace. 
+ * if you want to have the plugin to have it's own namespace.
  *
  *   Example use: OPAQ_REGISTER_PLUGIN(OPAQ::ExampleComponent);
  */
@@ -44,12 +45,12 @@ class ComponentNotFoundException;
 class PluginNotFoundException;
 class BadConfigurationException;
 
-/** 
-   * \brief class for managing the components. 
+/**
+   * \brief class for managing the components.
    * \author Stijn Van Looy
    *
-   *  Class to manage the components in OPAQ. It provides functionality to discover and 
-   *  create instances of the different OPAQ plugins which derive from OPAQ::Component. 
+   *  Class to manage the components in OPAQ. It provides functionality to discover and
+   *  create instances of the different OPAQ plugins which derive from OPAQ::Component.
    *
    *  This factory class was started from and loosely inspired by:
    *  http://stackoverflow.com/questions/582331/is-there-a-way-to-instantiate-objects-from-a-string-holding-their-class-name
@@ -85,7 +86,7 @@ public:
 private:
     typedef Component* (*FactoryFunc)(LogConfiguration*);
     typedef std::map<std::string, std::unique_ptr<Component>> InstanceMapType;
-    typedef std::map<std::string, FactoryFunc> FactoryMapType;
+    typedef std::map<std::string, boost::shared_ptr<FactoryFunc>> FactoryMapType;
 
     InstanceMapType instanceMap;
     FactoryMapType factoryMap;
