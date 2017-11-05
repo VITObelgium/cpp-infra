fd6fa77543367af94295f5948bf2a2486bf38d77
Dirk Vanden Boer <dirk.vdb@gmail.com>
2017-11-05 17:17:52 +0100

    Clang-tidy fixes

--- include/infra/geometadata.h
+++ include/infra/geometadata.h
@@ -15,19 +15,19 @@ struct GeoMetadata
 
     bool operator==(const GeoMetadata& other) const noexcept;
     bool operator!=(const GeoMetadata& other) const noexcept;
-    double convertXtoColFraction(const double x) const;
-    double convertYtoRowFraction(const double y) const;
-    int32_t convertXtoCol(const double x) const;
-    int32_t convertYtoRow(const double y) const;
-    double convertColCentreToX(const int32_t col) const;
-    double convertRowCentreToY(const int32_t row) const;
-    double convertColLLToX(const int32_t col) const;
-    double convertRowLLToY(const int32_t row) const;
-    Cell<int32_t> convertXYtoCell(const double x, const double y) const;
-    bool isXY(const double x, const double y, const int32_t row, const int32_t col) const;
+    double convertXtoColFraction(double x) const;
+    double convertYtoRowFraction(double y) const;
+    int32_t convertXtoCol(double x) const;
+    int32_t convertYtoRow(double y) const;
+    double convertColCentreToX(int32_t col) const;
+    double convertRowCentreToY(int32_t row) const;
+    double convertColLLToX(int32_t col) const;
+    double convertRowLLToY(int32_t row) const;
+    Cell<int32_t> convertXYtoCell(double x, double y) const;
+    bool isXY(double x, double y, int32_t row, int32_t col) const;
     bool isOnMap(const Cell<int32_t>& cell) const;
-    bool isOnMap(const int32_t r, const int32_t c) const;
-    void computeRectOnMapAround(const int32_t row, const int32_t col, const int32_t radius, int32_t& r0, int32_t& c0, int32_t& r1, int32_t& c1) const;
+    bool isOnMap(int32_t r, int32_t c) const;
+    void computeRectOnMapAround(int32_t row, int32_t col, int32_t radius, int32_t& r0, int32_t& c0, int32_t& r1, int32_t& c1) const;
 
     std::string toString() const;
 
