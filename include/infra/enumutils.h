d99ba1c56042fdd769a87eb2a47bb9986d164695
Dirk Vanden Boer <dirk.vdb@gmail.com>
2017-11-18 17:50:12 +0100

    enum_count added to enumutils

--- include/infra/enumutils.h
+++ include/infra/enumutils.h
@@ -2,8 +2,7 @@
 
 #include <type_traits>
 
-namespace infra
-{
+namespace infra {
 
 template <typename EnumType>
 constexpr auto enum_value(EnumType e)
@@ -11,4 +10,12 @@ constexpr auto enum_value(EnumType e)
     return static_cast<typename std::underlying_type_t<EnumType>>(e);
 }
 
+// Calling this function only makes sense for enums that use the convention
+// of adding a last enum field with the name EnumCount and where the other
+// enum fields do not explicitely set their value
+template <typename EnumType>
+constexpr std::underlying_type_t<EnumType> enum_count()
+{
+    return enum_value(EnumType::EnumCount);
+}
 }
