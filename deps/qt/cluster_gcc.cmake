e98489557129b56490e685143a2a37f6acbfe7e2
Dirk Vanden Boer <dirk.vdb@gmail.com>
2017-12-19 19:44:27 +0100

    Remove deps

--- deps/qt/cluster_gcc.cmake
+++
@@ -1,6 +0,0 @@
-message(STATUS "Patching QT for static runtime linking in: " ${QT_SRC_ROOT})
-
-file(READ ${QT_SRC_ROOT}/qt/qtbase/mkspecs/common/g++-base.conf conf_file)
-string(REPLACE "QMAKE_CC                = gcc" "QMAKE_CC                = x86_64-unknown-linux-gnu-gcc" conf_file "${conf_file}")
-string(REPLACE "QMAKE_CXX               = g++" "QMAKE_CXX               = x86_64-unknown-linux-gnu-g++" conf_file "${conf_file}")
-file(WRITE ${QT_SRC_ROOT}/qt/qtbase/mkspecs/common/g++-base.conf ${conf_file})
--- deps/toolchain-native.cmake
+++
@@ -1,3 +0,0 @@
-# for libraries and headers in the target directories
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
