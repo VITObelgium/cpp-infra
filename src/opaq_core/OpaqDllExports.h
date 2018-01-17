e1108d15f4e1e2a9635254b6300825b7f274c8b9
Dirk Vanden Boer <dirk.vdb@gmail.com>
2018-01-17 09:44:35 +0100

    Explicitely export symbols when using gcc

--- src/opaq_core/OpaqDllExports.h
+++ src/opaq_core/OpaqDllExports.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #ifndef WIN32
-#define OPAQ_DLL_API extern "C"
-#define OPAQ_DLL_CLASS extern "C"
+#define OPAQ_DLL_API extern "C" __attribute__((visibility("default")))
+#define OPAQ_DLL_CLASS extern "C" __attribute__((visibility("default")))
 #else
 #define OPAQ_DLL_API extern "C" __declspec(dllexport)
 #define OPAQ_DLL_CLASS extern "C" __declspec(dllexport)
