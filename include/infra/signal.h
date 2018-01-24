d2f1288dbcfd60b92c91fd9d46ae16e57ae5aab3
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2018-01-24 10:53:06 +0100

    Signal class added

---
+++ include/infra/signal.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <algorithm>
+#include <functional>
+#include <mutex>
+#include <vector>
+
+namespace infra {
+
+template <typename... Args>
+class Signal
+{
+public:
+    Signal()              = default;
+    Signal(const Signal&) = delete;
+    Signal(Signal&& other) noexcept
+    {
+        // move the other subscribers using his mutex
+        std::lock_guard<std::mutex> lock(other._mutex);
+        _subscribers = std::move(other._subscribers);
+    }
+
+    ~Signal()
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _subscribers.clear();
+    }
+
+    Signal& operator=(const Signal&) = delete;
+
+    Signal& operator=(Signal&& other) noexcept
+    {
+        // move the other subscribers using his mutex
+        std::lock_guard<std::mutex> lock(other._mutex);
+        _subscribers = std::move(other._subscribers);
+        return *this;
+    }
+
+    void connect(const void* receiver, const std::function<void(Args...)>& func)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _subscribers.push_back(Subscriber(receiver, func));
+    }
+
+    void disconnect(const void* receiver)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        auto iter = std::find_if(_subscribers.begin(), _subscribers.end(), [=](const auto& sub) {
+            return sub.receiver == receiver;
+        });
+
+        if (iter != _subscribers.end()) {
+            _subscribers.erase(iter);
+        }
+    }
+
+    template <typename... CallArgs>
+    void operator()(CallArgs&&... args)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        for (auto& sub : _subscribers) {
+            sub.callback(std::forward<CallArgs>(args)...);
+        }
+    }
+
+private:
+    struct Subscriber
+    {
+        Subscriber(const void* rcv, const std::function<void(Args...)>& cb)
+        : receiver(rcv)
+        , callback(cb)
+        {
+        }
+
+        const void* receiver;
+        std::function<void(Args...)> callback;
+    };
+
+    std::vector<Subscriber> _subscribers;
+    std::mutex _mutex;
+};
+}
