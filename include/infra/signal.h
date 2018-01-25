d81b32e73084fb38f4a5e881ea415f6f48563139
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2018-01-25 15:16:53 +0100

    Support signals without arguments

--- include/infra/signal.h
+++ include/infra/signal.h
@@ -79,4 +79,76 @@ private:
     std::vector<Subscriber> _subscribers;
     std::mutex _mutex;
 };
+
+template <>
+class Signal<>
+{
+public:
+    Signal()              = default;
+    Signal(const Signal&) = delete;
+    Signal(Signal&& other) noexcept
+    {
+        // move the other subscribers using his mutex
+        std::lock_guard<std::mutex> lock(other._mutex);
+        _subscribers = std::move(other._subscribers);
+    }
+
+    ~Signal()
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _subscribers.clear();
+    }
+
+    Signal& operator=(const Signal&) = delete;
+
+    Signal& operator=(Signal&& other) noexcept
+    {
+        // move the other subscribers using his mutex
+        std::lock_guard<std::mutex> lock(other._mutex);
+        _subscribers = std::move(other._subscribers);
+        return *this;
+    }
+
+    void connect(const void* receiver, const std::function<void()>& func)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        _subscribers.push_back(Subscriber(receiver, func));
+    }
+
+    void disconnect(const void* receiver)
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        auto iter = std::find_if(_subscribers.begin(), _subscribers.end(), [=](const auto& sub) {
+            return sub.receiver == receiver;
+        });
+
+        if (iter != _subscribers.end()) {
+            _subscribers.erase(iter);
+        }
+    }
+
+    void operator()()
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        for (auto& sub : _subscribers) {
+            sub.callback();
+        }
+    }
+
+private:
+    struct Subscriber
+    {
+        Subscriber(const void* rcv, const std::function<void()>& cb)
+        : receiver(rcv)
+        , callback(cb)
+        {
+        }
+
+        const void* receiver;
+        std::function<void()> callback;
+    };
+
+    std::vector<Subscriber> _subscribers;
+    std::mutex _mutex;
+};
 }
