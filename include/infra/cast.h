19390ae6880b154325eed426a54874be36461d47
Dirk Vanden Boer <dirk.vdb@gmail.com>
2018-01-27 19:38:08 +0100

    Simple ini parser added

--- include/infra/cast.h
+++ include/infra/cast.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include <type_traits>
 
 namespace infra {
@@ -9,5 +10,15 @@ TDest truncate(TSrc value)
 {
     static_assert(std::is_arithmetic_v<TSrc>, "Only use truncate on arithmetic types");
     return static_cast<TDest>(value);
+}
+
+template <class T, class U>
+std::optional<T> optional_cast(const std::optional<U>& u)
+{
+    if (u.has_value()) {
+        return std::make_optional<T>(static_cast<T>(*u));
+    } else {
+        return std::optional<T>();
+    }
 }
 }
