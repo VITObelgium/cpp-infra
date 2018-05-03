4286adad8a8246fa804bd30242066029f0f3f5ee
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2018-05-03 16:50:35 +0200

    Update to c++17 due to infra dependency

--- src/plugins/forecast/layer.h
+++ src/plugins/forecast/layer.h
@@ -3,52 +3,53 @@
 
 #include <functional>
 
-#include <tinyxml.h>
 #include <Eigen/Dense>
+#include <tinyxml.h>
 
 #include "transfcn.h"
 
-namespace nnet {
+namespace nnet
+{
 
-  // base class for a neural network layer
-  class layer {
-  public:
-    layer( TiXmlElement *cnf, int ninputs );
+// base class for a neural network layer
+class layer
+{
+public:
+    layer(TiXmlElement* cnf, int ninputs);
     virtual ~layer();
 
-    friend std::ostream& operator<<(std::ostream& os, const layer& lyr );
+    friend std::ostream& operator<<(std::ostream& os, const layer& lyr);
 
     int size() { return static_cast<int>(_a.size()); }
     std::string name() { return _Name; }
-    double* values(){ return _a.data(); } // the .data() method contains a const pointer to the scalar data
-    Eigen::VectorXd& valuesAsVector() { return _a; } 
+    double* values() { return _a.data(); } // the .data() method contains a const pointer to the scalar data
+    Eigen::VectorXd& valuesAsVector() { return _a; }
 
     int inputSize() { return static_cast<int>(_LW.cols()); }
     int outputSize() { return static_cast<int>(_LW.rows()); }
 
-    std::string fcnName(){ return _fcnName; } // transfer function
-    
+    std::string fcnName() { return _fcnName; } // transfer function
+
     // propagates an input vector through this layer
-    void compute( const Eigen::VectorXd &input ); 
+    void compute(const Eigen::VectorXd& input);
 
     // previous and next layers
-    layer *prev;
-    layer *next;
+    layer* prev;
+    layer* next;
 
-  private:
-    std::string     _Name; // layer name
+private:
+    std::string _Name; // layer name
 
     Eigen::VectorXd _a;  // layer values
     Eigen::MatrixXd _LW; // layer weights : from previous to this layer
     Eigen::VectorXd _B;  // layer bias vector
-    
+
     // the transfer function :
     // might think of encapsulating this in a class, but a bit overkill
-    std::pointer_to_unary_function<double,double> _fcn;  // function pointer
-    std::string _fcnName;  // name
-  };
-
+    std::function<double(double)> _fcn; // function pointer
+    std::string _fcnName;               // name
 };
 
+}; // namespace nnet
 
 #endif /* #ifndef __NNLAYER_H */
