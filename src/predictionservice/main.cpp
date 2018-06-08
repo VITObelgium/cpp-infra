416fa4adabad266476cfc831c26ed05a9161692b
Dirk Vanden Boer <dirk.vdb@gmail.com>
2018-06-08 15:44:14 +0200

    Improved service handler skeleton

--- src/predictionservice/main.cpp
+++ src/predictionservice/main.cpp
@@ -1,54 +1,68 @@
 #include <iostream>
 
+#include <memory>
 #include <restinio/all.hpp>
+
+#include "infra/log.h"
 
 namespace rest = restinio;
 
-// Create request handler.
-rest::request_handling_status_t handler(rest::request_handle_t req)
-{
-    if (rest::http_method_get() == req->header().method()) {
-        std::ostringstream sout;
-        sout << "GET request to '" << req->header().request_target() << "'\n";
+using infra::Log;
+using Router = rest::router::express_router_t<>;
 
+template <typename ResponseType>
+ResponseType initResponse(ResponseType resp)
+{
+    resp.append_header(rest::http_field::server, "OPAQ prediction server");
+    resp.append_header_date_field();
+    return resp;
+};
+
+auto createRequestHandler()
+{
+    auto router = std::make_unique<Router>();
+    router->http_get("/", [](auto req, auto) {
+        return initResponse(req->create_response())
+            .append_header(rest::http_field::content_type, "text/plain; charset=utf-8")
+            .set_body("OPAQ prediction server")
+            .done();
+    });
+
+    router->http_get("/getprediction", [](auto req, auto) {
         // Query params.
         const auto qp = rest::parse_query(req->header().query());
+        Log::info("Prediction query: date='{}' model='{}'", qp.get_param("date").value_or("None"), qp.get_param("model").value_or("None"));
 
-        if (0 == qp.size()) {
-            sout << "No query parameters.";
-        } else {
-            sout << "Query params (" << qp.size() << "):\n";
-
-            for (const auto p : qp) {
-                sout << "'" << p.first << "' => " << p.second << "'\n";
-            }
-        }
-
-        if (qp.has("debug") && qp["debug"] == "true") {
-            std::cout << sout.str() << std::endl;
-        }
-
-        req->create_response()
-            .append_header(rest::http_field::server, "OPAQ prediction server")
-            .append_header_date_field()
-            .append_header(rest::http_field::content_type, "text/plain; charset=utf-8")
-            .set_body(sout.str())
+        return initResponse(req->create_response())
+            .append_header(rest::http_field::content_type, "text/json; charset=utf-8")
+            .set_body("{ prediction-response: 'value' }")
             .done();
+    });
 
-        return rest::request_accepted();
-    }
+    router->non_matched_request_handler([](auto req) {
+        return req->create_response(400, "Bad Request")
+            .append_header_date_field()
+            .connection_close()
+            .done();
+    });
 
-    return rest::request_rejected();
+    return router;
 }
 
 int main()
 {
     try {
-        rest::run(
-            rest::on_thread_pool(std::thread::hardware_concurrency())
-                .port(8080)
-                .address("localhost")
-                .request_handler(handler));
+        Log::addConsoleSink(Log::Colored::On);
+        infra::LogRegistration logging("predictionservice");
+        Log::setLevel(Log::Level::Info);
+
+        // logging trait: rest::single_threaded_ostream_logger_t
+        using traits_t = rest::traits_t<rest::asio_timer_manager_t, rest::null_logger_t, Router>;
+
+        rest::run(rest::on_this_thread<traits_t>()
+                      .port(8080)
+                      .address("localhost")
+                      .request_handler(createRequestHandler()));
     } catch (const std::exception& ex) {
         std::cerr << "Error: " << ex.what() << std::endl;
         return EXIT_FAILURE;
