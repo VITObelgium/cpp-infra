82ce3f69eaffd2e1b88594f4b6f4b1f074c255e4
Dirk Vanden Boer <dirk.vdb@gmail.com>
2018-10-08 08:56:58 +0200

    Squashed 'deps/infra/' changes from 7420b7e..023a53b
    023a53b colormap and color combobox added
    05b520a ui containerconversions added
    
    git-subtree-dir: deps/infra
    git-subtree-split: 023a53b992542034da4a3248ea13da0b743c1875

---
+++ include/uiinfra/coloredrectangledelegate.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <optional>
+#include <qhash.h>
+#include <qstyleditemdelegate.h>
+
+class QModelIndex;
+class QWidget;
+class QVariant;
+
+namespace uiinfra {
+
+/*! Item delegate that draws a rectangle with the configured role color */
+class ColoredRectangleDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+public:
+    ColoredRectangleDelegate(QObject* parent = nullptr);
+
+    void setHeight(int height);
+    void setWidth(int width);
+
+    /* set modelindex role to use to obtain the color info (default = Qt::BackgroundColorRole) */
+    void setRole(int role);
+    int role() const;
+
+    void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+
+private:
+    QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const;
+
+    int _height = 20;
+    std::optional<int> _width;
+    int _role = Qt::BackgroundColorRole;
+};
+}
