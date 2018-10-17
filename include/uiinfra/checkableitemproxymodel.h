b8ce0b7c537b2473ffd85f4403b6841786c7c876
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2018-10-17 17:21:34 +0200

    CheckableItemProxyModel added

---
+++ include/uiinfra/checkableitemproxymodel.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <qhash.h>
+#include <qidentityproxymodel.h>
+
+namespace uiinfra {
+
+/*! Proxy model that make sure sure items are user checkable
+ * and display a checkbox
+ */
+class CheckableItemProxyModel : public QIdentityProxyModel
+{
+    Q_OBJECT
+
+public:
+    CheckableItemProxyModel(QObject* parent = nullptr);
+
+    Qt::ItemFlags flags(const QModelIndex& index) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole) override;
+
+private:
+    QHash<QModelIndex, Qt::CheckState> _checkStates;
+};
+}
