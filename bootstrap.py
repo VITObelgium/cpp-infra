150326411cc42df5adb1b20da124bf1f99e33ecb
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-01-04 17:17:14 +0100

    Support bootstrapping with the static runtime on windows

--- bootstrap.py
+++ bootstrap.py
@@ -2,13 +2,23 @@
 import os
 import sys
 import platform
+import argparse
 
 sys.path.insert(0, os.path.join(".", "deps", "vcpkg", "scripts"))
 from buildtools import vcpkg
 
 if __name__ == "__main__":
     try:
+        parser = argparse.ArgumentParser(description="Bootstrap opaq.")
+        parser.add_argument(
+            "--static", dest="static_runtime", default=False, action="store_true", help="Use the static runtime (windows only)"
+        )
+        args = parser.parse_args()
+
         triplet = "x64-windows" if platform.system() == "Windows" else None
+        if triplet and args.static_runtime:
+            triplet += "-static"
+
         if len(sys.argv) == 2 and sys.argv[1] == "upgrade":
             vcpkg.upgrade(triplet=triplet)
         else:
