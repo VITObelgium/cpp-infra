ed48a692d3aae8735dbd257939190e4e638fafa3
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-01-04 17:19:59 +0100

    Support building with the static runtime on windows

--- build.py
+++ build.py
@@ -17,6 +17,10 @@ if __name__ == "__main__":
             "--dist", dest="build_dist", action="store_true", help="build a release with proper git commit hash"
         )
 
+        parser.add_argument(
+            "--static", dest="static_runtime", default=False, action="store_true", help="Use the static runtime (windows only)"
+        )
+
         args = parser.parse_args()
 
         build_ui = "ON" if platform.system() == "Windows" or args.ui_enabled else "OFF"
@@ -24,16 +28,19 @@ if __name__ == "__main__":
 
         triplet = None
         if sys_platform == "win-amd64":
-            triplet = "x64-windows"
+            triplet = "x64-windows-static" if args.static_runtime else "x64-windows"
         elif sys_platform == "mingw":
             triplet = "x64-mingw"
 
-        cmake_args = ["-DOPAQ_ENABLE_UI={}".format(build_ui), "-DSTATIC_QT=OFF"]
+        cmake_args = ["-DOPAQ_ENABLE_UI={}".format(build_ui)]
 
         if triplet == "x64-windows":
-            cmake_args.extend(["-DCMAKE_PREFIX_PATH=C:/Qt/5.12.0/msvc2017_64"])
-        elif triplet == "x64-mingw":
-            cmake_args.extend(["-DSTATIC_QT=ON"])
+            cmake_args.append("-DCMAKE_PREFIX_PATH=C:/Qt/5.12.0/msvc2017_64")
+        elif triplet == "x64-mingw" or triplet == "x64-windows-static":
+            cmake_args.append("-DSTATIC_QT=ON")
+
+        if triplet == "x64-windows-static":
+            cmake_args.append("-DOPAQ_STATIC_RUNTIME=ON")
 
         if args.build_dist:
             vcpkg.build_project_release(
