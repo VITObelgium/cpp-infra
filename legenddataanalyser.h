8f424dc0a1c57308a7cf82abfdb9678f5bf30d36
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-01-22 10:13:35 +0100

    Squashed 'deps/infra/' changes from 419cf6e4a..79b250c8b
    79b250c8b Fixed warnings
    b1ecf7af2 Support creating legend bounds without sample data
    f9703f694 Merge commit '6dfe15df00934d6d8ca9c57cdc3a8633d02a7524' into develop
    a19af6882 Pixmap image class for rendering images without an image provider
    
    git-subtree-dir: deps/infra
    git-subtree-split: 79b250c8be99e4d49e3dfff9afad243b9400438b

--- legenddataanalyser.h
+++ legenddataanalyser.h
@@ -9,19 +9,11 @@ namespace inf {
 class LegendDataAnalyser
 {
 public:
-    enum class ClassboundsCalculationResult
-    {
-        NotEnoughDataForThisScale,
-        FullClassBounds,
-        DataRequiredForDerivationOfMinMax,
-        BadLegendScaleType,
-    };
-
     LegendDataAnalyser(std::vector<float> sampleData);
 
     void set_number_of_classes(int n);
 
-    ClassboundsCalculationResult calculate_classbounds(LegendScaleType scaleType, double minValue = 0, double maxValue = -1);
+    void calculate_classbounds(LegendScaleType scaleType, double minValue = 0, double maxValue = -1);
     void calculate_statistics(LegendScaleType LegendScaleType, double minValue, double maxValue);
 
     bool has_data() const;
@@ -39,11 +31,14 @@ private:
     std::vector<int> _freqNr;       // # of occurrences of the values
     int _nClasses;
     std::vector<double> _classBounds; // size: _nClasses + 1
-    bool _classBoundsOk;              // is data inside classbounds array valid
 
     // statistics
     double _msw;        // mean of squared deviations within classes
     double _msb;        // mean of squared deviations between classes
     double _varByClass; // proportion of variance accounted for by classification
 };
+
+std::vector<double> calculate_classbounds(LegendScaleType scaleType, int numClasses, double minValue, double maxValue);
+std::vector<double> calculate_classbounds(LegendScaleType scaleType, int numClasses, double minValue, double maxValue, const std::vector<float>& sampleData);
+
 }
