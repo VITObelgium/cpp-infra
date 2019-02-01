254a57c62f052b6f43172223dd2a1287128c5de7
Dirk Vanden Boer <dirk.vdb@gmail.com>
2019-02-01 10:00:32 +0100

    Fixed warning

--- cpuinfo.cpp
+++ cpuinfo.cpp
@@ -22,7 +22,7 @@ static void run_cpuid(uint32_t eax, uint32_t ecx, uint32_t* abcd)
 #if defined(_MSC_VER)
     __cpuidex(reinterpret_cast<int*>(abcd), eax, ecx);
 #else
-    uint32_t ebx, edx = 0;
+    uint32_t ebx = 0, edx = 0;
 #if defined(__i386__) && defined(__PIC__)
     /* in case of PIC under 32-bit EBX cannot be clobbered */
     __asm__("movl %%ebx, %%edi \n\t cpuid \n\t xchgl %%ebx, %%edi"
@@ -31,7 +31,7 @@ static void run_cpuid(uint32_t eax, uint32_t ecx, uint32_t* abcd)
     __asm__("cpuid"
             : "+b"(ebx),
 #endif
-              "+a"(eax), "+c"(ecx), "=d"(edx));
+            "+a"(eax), "+c"(ecx), "=d"(edx));
     abcd[0] = eax;
     abcd[1] = ebx;
     abcd[2] = ecx;
