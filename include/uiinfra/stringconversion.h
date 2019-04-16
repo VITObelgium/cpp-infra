488904da584e8be3746df089d5e0b1d1b790874b
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-04-16 14:57:05 +0200

    Helper function to convert arrays of std::string to QStringList

--- include/uiinfra/stringconversion.h
+++ include/uiinfra/stringconversion.h
@@ -2,14 +2,26 @@
 
 #include "infra/cast.h"
 
+#include <gsl/span>
 #include <qstring.h>
+#include <qstringlist.h>
 #include <string_view>
 
-namespace uiinfra {
+namespace inf::ui {
 
 inline QString toQString(std::string_view sv)
 {
     return QString::fromUtf8(sv.data(), inf::truncate<int>(sv.size()));
+}
+
+inline QStringList stringListFromArray(gsl::span<const std::string> strings)
+{
+    QStringList result;
+    for (auto s : strings) {
+        result.push_back(QString::fromStdString(s));
+    }
+
+    return result;
 }
 
 }
