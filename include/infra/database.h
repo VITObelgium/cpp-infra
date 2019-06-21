215342bc566727f62475515c95f1ef99bec95c1f
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-06-21 16:17:38 +0200

    Database functionality added

---
+++ include/infra/database.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <optional>
+#include <sqlpp11/sqlpp11.h>
+
+#ifdef INFRA_DB_SQLITE_SUPPORT
+#include "infra/filesystem.h"
+#include <sqlpp11/sqlite3/connection.h>
+#endif
+
+namespace inf::db {
+
+enum class AccessMode
+{
+    ReadOnly,
+    ReadWrite,
+    Create
+};
+
+template <typename DbConnectionType>
+class Transaction
+{
+public:
+    Transaction(Transaction&&)      = default;
+    Transaction(const Transaction&) = delete;
+    Transaction& operator=(const Transaction&) = delete;
+
+    Transaction(DbConnectionType& db)
+    : _transaction(start_transaction(db))
+    {
+    }
+
+    void commit()
+    {
+        _transaction.commit();
+    }
+
+    void rollback()
+    {
+        _transaction.rollback();
+    }
+
+private:
+    sqlpp::transaction_t<DbConnectionType> _transaction;
+};
+
+template <typename ResultField>
+auto optional_record_value(ResultField&& value)
+{
+    using ValueType = decltype(value.value());
+
+    if (value.is_null()) {
+        return std::optional<ValueType>();
+    }
+
+    return std::make_optional<ValueType>(value);
+}
+
+inline auto optional_field_value(const std::optional<double>& opt)
+{
+    if (opt.has_value()) {
+        return sqlpp::value_or_null(opt.value());
+    } else {
+        return sqlpp::value_or_null<sqlpp::floating_point>(sqlpp::null);
+    }
+}
+
+inline auto optional_field_value(const std::optional<int64_t>& opt)
+{
+    if (opt.has_value()) {
+        return sqlpp::value_or_null(opt.value());
+    } else {
+        return sqlpp::value_or_null<sqlpp::integer>(sqlpp::null);
+    }
+}
+
+#ifdef INFRA_DB_SQLITE_SUPPORT
+sqlpp::sqlite3::connection_config create_sqlite_connection_config(const fs::path& filename, inf::db::AccessMode access, bool debugQueries);
+#endif
+}
