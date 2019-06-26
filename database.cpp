30e6e4f93eff2580280a02d2c6b9d449875e0573
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-06-26 17:38:16 +0200

    Query log added

--- database.cpp
+++ database.cpp
@@ -2,8 +2,28 @@
 
 namespace inf::db {
 
+SerializerContext::SerializerContext(const SerializerContext& rhs)
+{
+    _os << rhs._os.str();
+}
+
+std::string SerializerContext::str() const
+{
+    return _os.str();
+}
+
+void SerializerContext::reset()
+{
+    _os.str("");
+}
+
+std::string SerializerContext::escape(std::string arg)
+{
+    return sqlpp::serializer_context_t::escape(arg);
+}
+
 #ifdef INFRA_DB_SQLITE_SUPPORT
-sqlpp::sqlite3::connection_config create_sqlite_connection_config(const fs::path& filename, inf::db::AccessMode access, bool debugQueries)
+sqlpp::sqlite3::connection_config create_sqlite_connection_config(const fs::path& filename, inf::db::AccessMode access, ConnectionDebug debug)
 {
     sqlpp::sqlite3::connection_config config;
     config.path_to_database = filename.string();
@@ -16,9 +36,8 @@ sqlpp::sqlite3::connection_config create_sqlite_connection_config(const fs::path
         config.flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
     }
 
-    config.debug = debugQueries;
+    config.debug = debug == ConnectionDebug::Yes;
     return config;
 }
 #endif
-
 }
