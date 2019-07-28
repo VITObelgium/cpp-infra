efc4ba65ef39242a9fb61ca1d9a3708eb134b5a2
Dirk Vanden Boer <dirk.vdb@gmail.com>
2019-07-28 13:06:45 +0200

    Fixed warnigns

--- include/uiinfra/doublespinboxwithinfinites.h
+++ include/uiinfra/doublespinboxwithinfinites.h
@@ -5,10 +5,11 @@
 
 namespace inf {
 
-class DoubleSpinBoxWithInfinites : public QDoubleSpinBox {
+class DoubleSpinBoxWithInfinites : public QDoubleSpinBox
+{
 public:
-
-    class ItemEditorCreator : public QItemEditorCreatorBase {
+    class ItemEditorCreator : public QItemEditorCreatorBase
+    {
         virtual QWidget* createWidget(QWidget* parent) const override;
         virtual QByteArray valuePropertyName() const override;
     };
@@ -17,10 +18,8 @@ public:
 
     virtual void fixup(QString& input) const override;
     virtual QValidator::State validate(QString& text, int& pos) const override;
-    virtual QString	textFromValue(double value) const;
-    virtual double valueFromText(const QString &text) const override;
-
-
+    virtual QString textFromValue(double value) const override;
+    virtual double valueFromText(const QString& text) const override;
 };
 
 }
