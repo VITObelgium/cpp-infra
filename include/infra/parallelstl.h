12f0e3b4ac324e33d363e60a5dcb6a279811b551
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-08-08 13:25:49 +0200

    parallelstl header that hides the complexity of using pstl on different compilers

---
+++ include/infra/parallelstl.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// We prefer to use the tbb runtime for the parallel stl implementation
+// so we can register thread start and stop callback
+// gcc uses intels pstl implementation under the hood
+// which depends on tbb, so that's fine
+// MSVC does not rely on the tbb so we use pstl manually in that case
+// and do not rely on the std implentation
+#if __has_include(<execution>) && !defined(_MSC_VER)
+#include <algorithm>
+#include <execution>
+#else
+#include <pstl/algorithm>
+#include <pstl/execution>
+#ifdef _MSC_VER
+namespace std::execution {
+using namespace __pstl::execution;
+}
+#endif
+#endif
