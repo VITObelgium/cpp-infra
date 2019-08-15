7ae5eab623b068165138962ea0fda5f785886fdb
Dirk Vanden Boer <dirk.vdb@gmail.com>
2019-08-15 21:51:42 +0200

    interpolate header added

---
+++ include/infra/interpolate.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cmath>
+#include <cstdint>
+
+namespace inf {
+
+template <typename T>
+float linear_map_to_float(T value, T min, T max)
+{
+    assert(min < max);
+
+    const auto rangeWidth = static_cast<float>(max - min);
+    return static_cast<float>(value - min) / rangeWidth;
+}
+
+template <typename T>
+uint8_t linear_map_to_byte(T value, T start, T end, uint8_t mapStart, uint8_t mapEnd)
+{
+    if (value < start || value > end) {
+        return 0;
+    }
+
+    if (mapStart == mapEnd) {
+        return mapStart;
+    }
+
+    const auto rangeWidth = end - start;
+    const auto pos        = static_cast<float>(value - start) / static_cast<float>(rangeWidth);
+
+    const auto mapWidth = mapEnd - mapStart;
+    return static_cast<uint8_t>(std::round(mapStart + (mapWidth * pos)));
+}
+}
