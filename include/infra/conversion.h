8c19d9c14a4912e92c55664995524a277d294ad5
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-09-06 13:46:53 +0200

    to_point function added

--- include/infra/conversion.h
+++ include/infra/conversion.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "infra/cast.h"
 #include "infra/coordinate.h"
 #include "infra/point.h"
 
@@ -9,6 +10,12 @@ template <typename T>
 Coordinate to_coordinate(Point<T> point)
 {
     return Coordinate(point.y, point.x);
+}
+
+template <typename T = double>
+Point<T> to_point(Coordinate coord)
+{
+    return Point<T>(truncate<T>(coord.longitude), truncate<T>(coord.latitude));
 }
 
 }
