f6625660b82e2d473b34052e0480a2519acc56a3
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-10-04 16:20:18 +0200

    Support individual color entries in gdal color table

--- include/infra/gdalcolortable.h
+++ include/infra/gdalcolortable.h
@@ -9,18 +9,28 @@ namespace inf::gdal {
 class ColorTable
 {
 public:
-    ColorTable(const ColorMap& cm)
+    ColorTable()
     : _ct(GPI_RGB)
     {
+    }
+
+    ColorTable(const ColorMap& cm)
+    : ColorTable()
+    {
         for (int i = 0; i < 255; ++i) {
-            auto color = cm.get_color(uint8_t(i));
-            GDALColorEntry entry;
-            entry.c1 = color.r;
-            entry.c2 = color.g;
-            entry.c3 = color.b;
-            entry.c4 = color.a;
+            auto entry = convert_color(cm.get_color(uint8_t(i)));
             _ct.SetColorEntry(i, &entry);
         }
+    }
+
+    void set_color_entry(int i, inf::Color color)
+    {
+        GDALColorEntry entry;
+        entry.c1 = color.r;
+        entry.c2 = color.g;
+        entry.c3 = color.b;
+        entry.c4 = color.a;
+        _ct.SetColorEntry(i, &entry);
     }
 
     const GDALColorTable* get() const
@@ -29,6 +39,16 @@ public:
     }
 
 private:
+    static GDALColorEntry convert_color(inf::Color color) noexcept
+    {
+        GDALColorEntry entry;
+        entry.c1 = color.r;
+        entry.c2 = color.g;
+        entry.c3 = color.b;
+        entry.c4 = color.a;
+        return entry;
+    }
+
     GDALColorTable _ct;
 };
 
