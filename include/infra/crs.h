31c6f3f79e954b20cfff9134cc76a021c72c9d03
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-10-10 16:06:08 +0200

    Renamed epsg header to crs and provide type safe epsg type

--- include/infra/crs.h
+++ include/infra/crs.h
@@ -5,11 +5,22 @@
 #include <fmt/core.h>
 #include <limits>
 
-namespace inf::epsg {
+namespace inf::crs {
 
+namespace epsg {
+// Epsg integer values
 //! Web mercator (typically used as warp target, when displaying rasters on top of tile data)
 static inline constexpr int32_t WGS84Spherical   = 3857;
 static inline constexpr int32_t WGS84Projected   = 4326;
 static inline constexpr int32_t BelgianLambert72 = 31370;
+}
+
+/*! Use this enum when you care about type safety in your interfaces */
+enum class Epsg : int32_t
+{
+    WGS84Spherical   = epsg::WGS84Spherical,
+    WGS84Projected   = epsg::WGS84Projected,
+    BelgianLambert72 = epsg::BelgianLambert72,
+};
 
 }
