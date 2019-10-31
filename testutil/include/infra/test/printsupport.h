94195848b69c5765061fa9685c6e0865c931bc2a
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2019-10-31 15:19:12 +0100

    testutils library added with test print support

---
+++ testutil/include/infra/test/printsupport.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "infra/cell.h"
+#include "infra/point.h"
+#include <optional>
+
+namespace inf {
+void PrintTo(const Cell& cell, std::ostream* os);
+
+template <typename T>
+void PrintTo(const Point<T>& p, std::ostream* os)
+{
+    *os << fmt::format("{}", p);
+}
+}
+
+namespace std {
+template <typename T>
+void PrintTo(const optional<T>& opt, std::ostream* os)
+{
+    if (opt.has_value()) {
+        *os << opt.value();
+    } else {
+        *os << "no value";
+    }
+}
+}
