8dcd49eb6f2c5b7ffbcc50191f2e796ca6b9f237
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2020-02-20 11:36:03 +0100

    Use reproc for cross platform process launching functionality

--- include/infra/process.h
+++ include/infra/process.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <string_view>
+#include <string>
+#include <gsl/span>
 
 namespace inf {
 
-int run_process(std::string_view cmdLine);
+int run_process(gsl::span<const std::string> command);
 }
