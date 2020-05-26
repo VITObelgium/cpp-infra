51395a4cba5e74b5202756c78b0a3969aefd5b4c
Dirk Vanden Boer <dirk.vdb@gmail.com>
2020-05-26 15:46:46 +0200

    More options for running processes

--- include/infra/process.h
+++ include/infra/process.h
@@ -4,8 +4,46 @@
 
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace inf {
 
+struct ProcessRunOptions
+{
+    std::vector<std::pair<std::string, std::string>> environmentVariables;
+    std::string workingDirectory;
+};
+
+struct ProcessRunResult
+{
+    int status;
+    std::string stdout;
+    std::string stderr;
+};
+
+/*!
+    @brief Run a process
+    @param command array of executable path with arguments
+    @return the status code of the process
+    @throw RuntimeError in case of an error launching the process
+ */
 int run_process(std::span<const std::string> command);
+
+/*!
+    @brief Run a process with the option to tweak behavior
+    @param command array of executable path with arguments
+    @param opts process options
+    @return the status code of the process
+    @throw RuntimeError in case of an error launching the process
+ */
+int run_process(std::span<const std::string> command, const ProcessRunOptions& opts);
+
+/*!
+    @brief Run a process and capture the program output with the option to tweak behavior
+    @param command array of executable path with arguments
+    @param opts process options
+    @return a ProcessRunResult structure containing the status code and output an error stream results
+    @throw RuntimeError in case of an error launching the process
+ */
+ProcessRunResult run_process_capture_output(std::span<const std::string> command, const ProcessRunOptions& opts);
 }
--- process.cpp
+++ process.cpp
@@ -18,4 +18,54 @@ int run_process(std::span<const std::string> command)
 
     return status;
 }
+
+int run_process(std::span<const std::string> command, const ProcessRunOptions& opts)
+{
+    reproc::options options;
+    options.redirect.parent = true;
+    options.environment     = opts.environmentVariables;
+
+    if (!opts.workingDirectory.empty()) {
+        options.working_directory = opts.workingDirectory.c_str();
+    }
+
+    auto [status, ec] = reproc::run(command, options);
+    if (ec) {
+        throw RuntimeError("Failed to run process ({}).", ec.message());
+    }
+
+    return status;
+}
+
+ProcessRunResult run_process_capture_output(std::span<const std::string> command, const ProcessRunOptions& opts)
+{
+    ProcessRunResult result;
+
+    reproc::options options;
+    options.environment = opts.environmentVariables;
+
+    reproc::process process;
+    if (auto ec = process.start(command, options); ec) {
+        throw RuntimeError("Failed to start process ({}).", ec.message());
+    }
+
+    if (!opts.workingDirectory.empty()) {
+        options.working_directory = opts.workingDirectory.c_str();
+    }
+
+    reproc::sink::string outsink(result.stdout);
+    reproc::sink::string errsink(result.stderr);
+
+    if (auto drainec = reproc::drain(process, outsink, errsink); drainec) {
+        throw RuntimeError("Failed to read process output ({}).", drainec.message());
+    }
+
+    std::error_code ec;
+    std::tie(result.status, ec) = process.wait(reproc::infinite);
+    if (ec) {
+        throw RuntimeError("Failed to run process ({}).", ec.message());
+    }
+
+    return result;
+}
 }
