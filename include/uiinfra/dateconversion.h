62612f468b72311df27cb100cce940cd606916d1
Dirk Vanden Boer <dirk.vdb@gmail.com>
2020-06-25 17:09:44 +0200

    datePointFromDate function added

--- include/uiinfra/dateconversion.h
+++ include/uiinfra/dateconversion.h
@@ -18,4 +18,11 @@ QDateTime toDateTime(const std::chrono::time_point<Clock, Duration>& tp)
     auto time = date::make_time(tp - std::chrono::floor<date::days>(tp));
     return QDateTime(toDate(tp), QTime(time.hours().count(), time.minutes().count(), time.seconds().count()));
 }
+
+inline inf::chrono::date_point datePointFromDate(const QDate& date)
+{
+    const date::year_month_day ymd(date::year(date.year()),
+                                   date::month(date.month()),
+                                   date::day(date.day()));
+    return static_cast<date::sys_days>(ymd);
 }
