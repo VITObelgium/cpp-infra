3faafa1cd5295cde6de077819f80ffa476053360
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2020-09-21 10:45:21 +0200

    ErrorCode exception type added

--- include/infra/exception.h
+++ include/infra/exception.h
@@ -36,4 +36,36 @@ EXCEPTION(InvalidArgument, std::invalid_argument)
 EXCEPTION(LicenseError, std::runtime_error)
 EXCEPTION(RangeError, std::out_of_range)
 EXCEPTION(CancelRequested, std::runtime_error)
+
+template <typename EcEnum>
+class ErrorCodeException : public RuntimeError
+{
+public:
+    ErrorCodeException(EcEnum ec)
+    : _errorCode(ec)
+    {
+    }
+
+    ErrorCodeException(EcEnum ec, std::string_view message)
+    : RuntimeError(message)
+    , _errorCode(ec)
+    {
+    }
+
+    template <typename... Args>
+    ErrorCodeException(EcEnum ec, const char* formatStr, const Args&... args)
+    : RuntimeError(formatStr, std::forward<const Args&>(args)...)
+    , _errorCode(ec)
+    {
+    }
+
+    EcEnum error_code() const noexcept
+    {
+        return _errorCode;
+    }
+
+private:
+    EcEnum _errorCode;
+};
+
 }
