a5afd7448b038990715fb5923e973bd4d419d188
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2020-11-06 10:09:56 +0100

    truncate is now constexpr

--- include/infra/cast.h
+++ include/infra/cast.h
@@ -60,7 +60,7 @@ constexpr bool fits_in_type(TInput value)
 }
 
 template <typename TDest, typename TSrc>
-TDest truncate(TSrc value)
+constexpr TDest truncate(TSrc value)
 {
     static_assert(std::is_arithmetic_v<TSrc>, "Only use truncate on arithmetic types");
     return static_cast<TDest>(value);
