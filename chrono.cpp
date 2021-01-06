31b6e99e0631031351d70fa7afedbdc7b6ac432a
Dirk Vanden Boer <dirk.vdb@gmail.com>
2021-01-06 13:34:39 +0100

    Some chrono convenience functions

--- chrono.cpp
+++ chrono.cpp
@@ -12,9 +12,20 @@ date_point today()
     return date::year_month_day(date::sys_days(std::chrono::floor<date::days>(std::chrono::system_clock::now())));
 }
 
+time_point now()
+{
+    return std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now());
+}
+
 date_point date_from_time_point(time_point tp)
 {
     return std::chrono::floor<date::days>(tp);
+}
+
+date::hh_mm_ss<std::chrono::milliseconds> time_of_day(time_point tp)
+{
+    auto dp = date::floor<date::days>(tp);
+    return date::make_time(tp - dp); // Yields time_of_day type
 }
 
 date::year_month_day to_year_month_day(time_point tp)
--- include/infra/chrono.h
+++ include/infra/chrono.h
@@ -11,7 +11,10 @@ using date_point = std::chrono::time_point<std::chrono::system_clock, days>;
 using time_point = std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds>;
 
 date_point today();
+time_point now();
+
 date_point date_from_time_point(time_point tp);
+date::hh_mm_ss<std::chrono::milliseconds> time_of_day(time_point tp);
 date::year_month_day to_year_month_day(time_point tp);
 
 /*! Converts a year_month_day to a system timepoint (locale dependent) */
