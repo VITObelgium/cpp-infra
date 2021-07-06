7e1ec3dd93fb40b5ecc17264346039f5734eeb02
Dirk Vanden Boer <dirk.vdb@gmail.com>
2021-07-06 16:04:00 +0200

    Fixed deprecated function warning

--- gdalgeometry.cpp
+++ gdalgeometry.cpp
@@ -479,7 +479,7 @@ double MultiLine::length() const
 {
     double length = 0.0;
     for (int i = 0; i < size(); ++i) {
-        length += get()->getGeometryRef(i)->toLineString()->get_Length();
+        length += get()->getGeometryRef(i)->get_Length();
     }
 
     return length;
