0af35575aac46d632c310f2951fc48a9478cc60f
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2021-07-12 15:19:59 +0200

    Fixed setenv call

--- environmentvariable.cpp
+++ environmentvariable.cpp
@@ -10,7 +10,7 @@ void set(std::string_view key, std::string_view value)
 #ifdef WIN32
     _putenv_s(std::string(key).c_str(), std::string(value).c_str());
 #else
-    setenv(std::string(key).c_str(), std::string(value).c_str());
+    setenv(std::string(key).c_str(), std::string(value).c_str(), 1);
 #endif
 }
 
