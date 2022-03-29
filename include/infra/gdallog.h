690924e7c6c38f60814591fe52bbc2660544994d
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2022-03-29 13:39:39 +0200

    Support setting log level for gdal log interception

--- include/infra/gdallog.h
+++ include/infra/gdallog.h
@@ -3,4 +3,14 @@
 namespace inf::gdal {
 
 void set_log_handler();
+
 }
+
+#ifdef INFRA_LOG_ENABLED
+#include "infra/log.h"
+
+namespace inf::gdal {
+void set_log_handler(Log::Level levelToUse);
+}
+
+#endif
