c182e3f16a8701b31a6bac36c24194cb76b99d5d
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2023-02-02 11:42:10 +0100

    BelgianLambert2008 epsg code added

--- include/infra/crs.h
+++ include/infra/crs.h
@@ -15,11 +15,13 @@ namespace inf::crs {
 namespace epsg {
 // Epsg integer values
 //! Web mercator (typically used as warp target, when displaying rasters on top of tile data)
-static inline constexpr int32_t WGS84WebMercator = 3857;
-static inline constexpr int32_t WGS84            = 4326; // geographic projection
-static inline constexpr int32_t BelgianLambert72 = 31370;
-static inline constexpr int32_t Belge72Geo       = 4313; // geographic projection
-static inline constexpr int32_t ETRS89           = 3035;
+static inline constexpr int32_t WGS84WebMercator   = 3857;
+static inline constexpr int32_t WGS84              = 4326; // geographic projection
+static inline constexpr int32_t BelgianLambert72   = 31370;
+static inline constexpr int32_t Belge72Geo         = 4313; // geographic projection
+static inline constexpr int32_t ETRS89             = 3035;
+static inline constexpr int32_t BelgianLambert2008 = 3812;
+
 }
 
 /*! Use this enum when you care about type safety in your interfaces */
