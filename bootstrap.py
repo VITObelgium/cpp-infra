f9bf15fa57ae25028bec9cc390403aa4a5b7eec7
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2023-10-03 14:06:58 +0200

    Add missing tbb feature

--- bootstrap.py
+++ bootstrap.py
@@ -37,6 +37,7 @@ if __name__ == "__main__":
             "process",
             "hashing",
             "xml",
+            "tbb",
             "numeric",
             "charset",
             "compression",
