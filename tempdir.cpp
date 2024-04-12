51b172652d4720817677a84c49b7cac4f85ab37e
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2024-04-12 16:26:17 +0200

    TempDir handle the case where the temp dir does not exist

--- tempdir.cpp
+++ tempdir.cpp
@@ -10,7 +10,17 @@ TempDir::TempDir()
 TempDir::TempDir(std::string_view name)
 {
     const auto timestamp = std::chrono::high_resolution_clock::now().time_since_epoch().count();
-    _path                = fs::temp_directory_path() / std::to_string(timestamp);
+    try {
+        _path = fs::temp_directory_path() / std::to_string(timestamp);
+    } catch (const fs::filesystem_error& e) {
+        if (e.code() == std::errc::not_a_directory && !fs::exists(e.path1())) {
+            // The temp directory does not exist, so we need to create it.
+            fs::create_directories(e.path1());
+            _path = fs::temp_directory_path() / std::to_string(timestamp);
+        } else {
+            throw e;
+        }
+    }
 
     if (!name.empty()) {
         _path /= file::u8path(name);
