7cf2f318b7198892fc6dcfa2fc32510559059530
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2025-02-12 10:10:51 +0100

    Fix chrono test on gcc

--- test/chronotest.cpp
+++ test/chronotest.cpp
@@ -8,14 +8,14 @@ using namespace std::chrono_literals;
 
 TEST_CASE("ChronoTest.time_point_to_utc_string")
 {
-    auto date =  chrono::date_point(2017y / std::chrono::February / 1);
+    chrono::date_point date = std::chrono::sys_days(2017y / std::chrono::February / 1);
 
     CHECK("2017-02-01" == inf::chrono::to_string("%Y-%m-%d", date));
 }
 
 TEST_CASE("ChronoTest.time_point_to_string_custom_format")
 {
-    auto date = chrono::date_point(2017y / std::chrono::February / 1);
+    chrono::date_point date = std::chrono::sys_days(2017y / std::chrono::February / 1);
 
     CHECK("2017-02-01" == inf::chrono::to_string("%Y-%m-%d", date));
     CHECK("2017-01-02" == inf::chrono::to_string("%Y-%d-%m", date));
