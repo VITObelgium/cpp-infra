0cb75cb67a841aa00cb0abfd412bc61a96438296
Dirk Vanden Boer <dirk.vandenboer@vito.be>
2025-05-22 11:02:18 +0200

    Support triplet specification for dist targets

--- dist.bat
+++ dist.bat
@@ -5,8 +5,6 @@ REM Print the first argument of the script
 FOR /F "tokens=* USEBACKQ" %%F IN (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property InstallationPath`) DO (
 SET vspath=%%F
 )
-ECHO %vspath%
-ECHO %1
 set VSCMD_START_DIR=%1
 CALL "%vspath%\VC\Auxiliary\Build\vcvarsall.bat" x64
-just build_dist
+just build_dist %2
