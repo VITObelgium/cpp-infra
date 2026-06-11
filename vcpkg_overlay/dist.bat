d623c5fd7621f7f52fc4afd0eec6c9983910abbe
Dirk Vanden Boer <dirk.vdb@gmail.com>
2026-06-11 13:16:26 +0200

    Make dist.bat more generic

--- vcpkg_overlay/dist.bat
+++ vcpkg_overlay/dist.bat
@@ -1,12 +1,19 @@
 @echo off
 
-REM Print the first argument of the script
-
 FOR /F "tokens=* USEBACKQ" %%F IN (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property InstallationPath`) DO (
 SET vspath=%%F
 )
 set VSCMD_START_DIR=%1
-set GIT_COMMIT_HASH=%3
 echo "InstallationPath: %vspath%"
 CALL "%vspath%\VC\Auxiliary\Build\vcvarsall.bat" x64
-just build_dist %2
+
+REM Run the command passed as remaining arguments
+set "cmd="
+:buildcmd
+shift
+if "%~1"=="" goto runcmd
+if defined cmd (set "cmd=%cmd% %1") else (set "cmd=%1")
+goto buildcmd
+
+:runcmd
+%cmd%
